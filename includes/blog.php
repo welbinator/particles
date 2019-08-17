84afcb8141d45b57818ba1fb35273769ca810c95
welbinator <james.welbes@gmail.com>
2019-08-17 11:16:19 -0500

    initial commit

---
+++ includes/blog.php
@@ -0,0 +1,66 @@
+<!--== Blog Posts Start ==-->
+<section class="white-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 section-heading">
+          <h5 class="default-color text-uppercase">Latest Blogs</h5>
+          <h2 class="play-font font-700">Smart Marketing Tools That <span class="default-color">Grow</span><br> Your Business</h2>
+        </div>
+      </div>
+        <div class="row mt-50">
+          <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-delay="0.1s">
+            <div class="post">
+              <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-09.jpg" alt=""/> </div>
+              <div class="post-info pt-20 pb-20">
+                <h3><a href="blog-grid.html">4 Deadly Sins of UI and UX Design You Need to Avoid</a></h3>
+                <h6>January 15, 2018</h6>
+                <hr>
+                <p class="mt-10"> <span> <a class="extras-wrap" href="#"><i class="icofont icofont-chat"></i><span>0 Comments</span></a> <span class="extras-wrap"><i class="icofont icofont-clock-time"></i><span>5 Minutes</span></span> </span> </p>
+                <a class="readmore" href="#"><span>Read More</span></a> </div>
+            </div>
+          </div>
+          <!--== Post End ==-->
+
+          <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-delay="0.2s">
+            <div class="post">
+              <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-18.jpg" alt=""/> </div>
+              <div class="post-info pt-20 pb-20">
+                <h3><a href="blog-grid.html">20 Trends That Will Be Huge This May</a></h3>
+                <h6>April 19, 2014</h6>
+                <hr>
+                <p class="mt-10"> <span> <a class="extras-wrap" href="#"><i class="icofont icofont-chat"></i><span>20 Comments</span></a> <span class="extras-wrap"><i class="icofont icofont-clock-time"></i><span>8 Minutes</span></span> </span> </p>
+                <a class="readmore" href="#"><span>Read More</span></a> </div>
+            </div>
+          </div>
+          <!--== Post End ==-->
+
+          <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-delay="0.3s">
+            <div class="post">
+              <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-11.jpg" alt=""/> </div>
+              <div class="post-info pt-20 pb-20">
+                <h3><a href="blog-grid.html">This App Aims to Make Life a Little Easier for Plus-Size People</a></h3>
+                <h6>Febuary 13, 2018</h6>
+                <hr>
+                <p class="mt-10"> <span> <a class="extras-wrap" href="#"><i class="icofont icofont-chat"></i><span>8 Comments</span></a> <span class="extras-wrap"><i class="icofont icofont-clock-time"></i><span>Just Now</span></span> </span> </p>
+                <a class="readmore" href="#"><span>Read More</span></a> </div>
+            </div>
+          </div>
+          <!--== Post End ==-->
+
+          <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-delay="0.4s">
+            <div class="post">
+              <div class="post-img"> <img class="img-responsive" src="assets/images/post/post-15.jpg" alt=""/> </div>
+              <div class="post-info pt-20 pb-20">
+                <h3><a href="blog-grid.html">10 Unbelievably Tranquil and Beautiful Floating Hotels</a></h3>
+                <h6>November 18, 2016</h6>
+                <hr>
+                <p class="mt-10"> <span> <a class="extras-wrap" href="#"><i class="icofont icofont-chat"></i><span>88 Comments</span></a> <span class="extras-wrap"><i class="icofont icofont-clock-time"></i><span>15 Minutes</span></span> </span> </p>
+                <a class="readmore" href="#"><span>Read More</span></a> </div>
+            </div>
+          </div>
+          <!--== Post End ==-->
+
+        </div>
+    </div>
+  </section>
+  <!--== Blog Posts End ==-->
